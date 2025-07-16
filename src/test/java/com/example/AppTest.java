cd2676e14652c376c3c428781d700e41bc7c5671
tarun-asthana <60086012+tarun-asthana@users.noreply.github.com>
2025-07-16 20:23:22 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,1 @@
-package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class AppTest {
-    @Test
-    public void testHello() {
-        assertEquals("Hello, Jenkins!", App.hello());
-    }
-}
